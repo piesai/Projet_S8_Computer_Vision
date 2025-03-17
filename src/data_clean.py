88ba7bd28fad666dd86f11d8a5b8d120d7b88052
pierres <sainctavitp@gmail.com>
2025-03-17 16:24:16 +0100

    extraction des données

--- src/data_clean.py
+++ src/data_clean.py
@@ -1,13 +1,17 @@
+import torchvision
 import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchvision.transforms as v1
 import matplotlib.pyplot as plt
+import random as rd
+dataset = torchvision.datasets.GTSRB(root = '/home/pierres/Projet_S8/data/',
+                           download=True,
+                           transform = v1.Compose([v1.PILToTensor(),v1.Pad(243)]))
+
+print(dataset.__getitem__(2))
+for k in range(10):
+    m = rd.randint(0,len(dataset))
+    plt.imshow(dataset.__getitem__(m)[0].permute(1,2,0))
+    plt.title(dataset.__getitem__(m)[1])
+    plt.show()
 
 
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)   
--- src/visualisation.py
+++ src/visualisation.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cv2
 import tensorflow as tf
 import random as rd
+from data_clean import dataset
 L = []
 
 #affichage du nombre de fichier
@@ -12,6 +13,7 @@ for k in range(42):
     rep2 = '/home/pierres/Projet_S8/data/validation/' + str(k)
     nombre_de_fichier_2 = len([f for f in os.listdir(rep2) if os.path.isfile(os.path.join(rep2, f))])
     print(nombre_de_fichier_1,nombre_de_fichier_2)
+
 
 #essaye de la bibiliothéque cv2
 im = cv2.imread("/home/pierres/Projet_S8/data/train/0/00000_00005_00003.png")
@@ -29,11 +31,13 @@ for k in range(42):
     
 
 
+#affichage de 10 images aléatoires
+print(dataset.__getitem__(2))
+for k in range(10):
+    m = rd.randint(0,len(dataset))
+    plt.imshow(dataset.__getitem__(m)[0].permute(1,2,0))
+    plt.title(dataset.__getitem__(m)[1])
+    plt.show()
 
-print(im.shape,im2.shape)
 
-cv2.imshow('image',im)
-cv2.imshow('image2',im2)
-cv2.waitKey(0)
-L = []
-rep = '/home/pierres/Projet_S8/data/train/1/'
+
