a82d15af5575283ca689cefc6b56c3ed70afbdf7
pierres <sainctavitp@gmail.com>
2025-03-17 16:09:46 +0100

    début d'extraction propre données avec pytorch

--- src/data_clean.py
+++ src/data_clean.py
@@ -1,13 +1,14 @@
+import torchvision
 import torch
-from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
+import torchvision.transforms as v1
 import matplotlib.pyplot as plt
+dataset = torchvision.datasets.GTSRB(root = '/home/pierres/Projet_S8/data/',
+                           download=True,
+                           transform = v1.Compose([v1.PILToTensor(),v1.Pad(300)]))
 
+print(dataset.__getitem__(2))
 
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)   
+plt.plot(dataset.__getitem__(2))
+
+data_loader = torch.utils.data.DataLoader(dataset,batch_size =1,shuffle = True)
+
--- src/models.py
+++ src/models.py
@@ -1,9 +1,9 @@
-from data_clean import coucou
+
 import keras
 import numpy as np
 model = keras.Sequential()
 model.add(keras.Input(shape=(1,)))
-model.add(keras.layers.Dense(8))
+model.add(keras.layers.Dense(100))
 model.add(keras.layers.Dense(1))
 
 train = np.array([1 for k in range(100)] + [0 for k in range(100)])
@@ -14,4 +14,3 @@ model.fit(train,label,batch_size=2,epochs=10)
 
 a = model.predict(np.array([[0],[1]]))
 print(a)
-print(coucou)
