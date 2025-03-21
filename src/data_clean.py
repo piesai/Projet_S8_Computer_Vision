adaf9a9de4a55fea6dddc65b1ef23e0e3ccd7df6
pierres <sainctavitp@gmail.com>
2025-03-21 09:30:36 +0100

    bonne extraction

--- src/data_clean.py
+++ src/data_clean.py
@@ -1,17 +1,10 @@
 import torchvision
 import torch
-import torchvision.transforms as v1
+from  torchvision.transforms import v2
 import matplotlib.pyplot as plt
 import random as rd
 dataset = torchvision.datasets.GTSRB(root = '/home/pierres/Projet_S8/data/',
                            download=True,
-                           transform = v1.Compose([v1.PILToTensor(),v1.Pad(243)]))
-
-print(dataset.__getitem__(2))
-for k in range(10):
-    m = rd.randint(0,len(dataset))
-    plt.imshow(dataset.__getitem__(m)[0].permute(1,2,0))
-    plt.title(dataset.__getitem__(m)[1])
-    plt.show()
+                           transform = v2.Compose([v2.PILToTensor(),v2.Resize(size = (200,200))])) #quel padding mettre sachant que la plus grosse image va jusqu'à 243
 
 
--- src/visualisation.py
+++ src/visualisation.py
@@ -4,22 +4,14 @@ import cv2
 import tensorflow as tf
 import random as rd
 from data_clean import dataset
+import numpy as np
 L = []
 
 #affichage du nombre de fichier
 for k in range(42):
     rep = '/home/pierres/Projet_S8/data/train/' + str(k)
     nombre_de_fichier_1 = len([f for f in os.listdir(rep) if os.path.isfile(os.path.join(rep, f))])
-    rep2 = '/home/pierres/Projet_S8/data/validation/' + str(k)
-    nombre_de_fichier_2 = len([f for f in os.listdir(rep2) if os.path.isfile(os.path.join(rep2, f))])
-    print(nombre_de_fichier_1,nombre_de_fichier_2)
-
-
-#essaye de la bibiliothéque cv2
-im = cv2.imread("/home/pierres/Projet_S8/data/train/0/00000_00005_00003.png")
-cv2.resize(im,(32,32))
-im2 = cv2.imread("/home/pierres/Projet_S8/data/train/0/00000_00000_00003.png")
-cv2.resize(im2,(32,32))
+    print(nombre_de_fichier_1)
 
 
 #affichage de dimensions d'images
@@ -36,6 +28,8 @@ print(dataset.__getitem__(2))
 for k in range(10):
     m = rd.randint(0,len(dataset))
     plt.imshow(dataset.__getitem__(m)[0].permute(1,2,0))
+    a = np.array(dataset.__getitem__(m)[0]).shape
+    print(a)
     plt.title(dataset.__getitem__(m)[1])
     plt.show()
 
