98a8fc922c32b130582b5fa807028f0f78c4f1c0
pierres <sainctavitp@gmail.com>
2025-03-21 14:01:29 +0100

    85% accuracy

--- src/models.py
+++ src/models.py
@@ -1,8 +1,9 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from data_clean import trainloader
+from data_clean import trainloader,trainloader_test
 import torch
-
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Net(nn.Module):
     def __init__(self):
@@ -10,9 +11,9 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16, 35344)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        self.fc1 = nn.Linear(35344, 2048)
+        self.fc2 = nn.Linear(2048, 126)
+        self.fc3 = nn.Linear(126, 43)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -29,7 +30,7 @@ net = Net()
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+optimizer = optim.SGD(net.parameters(), lr=0.002, momentum=0.9)
 
 for epoch in range(2):  # loop over the dataset multiple times
 
@@ -37,7 +38,6 @@ for epoch in range(2):  # loop over the dataset multiple times
     for i, data in enumerate(trainloader, 0):
         # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-        print(data)
         # zero the parameter gradients
         optimizer.zero_grad()
 
@@ -49,8 +49,32 @@ for epoch in range(2):  # loop over the dataset multiple times
 
         # print statistics
         running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
+        if i % 10 == 0:    # print every 2000 mini-batches
+            
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
 
+
 print('Finished Training')
+
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+print(labels)
+outputs = net(images)
+outputs = outputs.detach().numpy()
+print([np.argmax(outputs[i]) for i in range(len(outputs))])
+
+correct = 0
+total = 0
+# since we're not training, we don't need to calculate the gradients for our outputs
+with torch.no_grad():
+    for data in trainloader_test:
+        images, labels = data
+        # calculate outputs by running images through the network
+        outputs = net(images)
+        # the class with the highest energy is what we choose as prediction
+        _, predicted = torch.max(outputs, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+print(f'Accuracy of the network on the 10000 test images: {100 * correct // total} %')
