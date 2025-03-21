984bc45fe62cc7a1b31d685ff4891b8babf08e84
pierres <sainctavitp@gmail.com>
2025-03-21 09:50:35 +0100

    extraction

--- src/data_clean.py
+++ src/data_clean.py
@@ -3,8 +3,14 @@ import torch
 from  torchvision.transforms import v2
 import matplotlib.pyplot as plt
 import random as rd
+import numpy as np
 dataset = torchvision.datasets.GTSRB(root = '/home/pierres/Projet_S8/data/',
                            download=True,
                            transform = v2.Compose([v2.PILToTensor(),v2.Resize(size = (200,200))])) #quel padding mettre sachant que la plus grosse image va jusqu'à 243
+
+
+batch_size = 16
+
+trainloader = torch.utils.data.DataLoader(dataset, batch_size=16,shuffle = True)
 
 
--- src/models.py
+++ src/models.py
@@ -1,17 +1,56 @@
-from data_clean import coucou
-import keras
-import numpy as np
-model = keras.Sequential()
-model.add(keras.Input(shape=(1,)))
-model.add(keras.layers.Dense(8))
-model.add(keras.layers.Dense(1))
+import torch.nn as nn
+import torch.nn.functional as F
+from data_clean import trainloader
+import torch
 
-train = np.array([1 for k in range(100)] + [0 for k in range(100)])
-label = np.array([1 for k in range(100)] + [0 for k in range(100)])
-model.compile(optimizer = 'sgd', loss = 'mse')
 
-model.fit(train,label,batch_size=2,epochs=10)
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
-a = model.predict(np.array([[0],[1]]))
-print(a)
-print(coucou)
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+net = Net()
+
+import torch.optim as optim
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+for epoch in range(2):  # loop over the dataset multiple times
+
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs; data is a list of [inputs, labels]
+        inputs, labels = data
+        print(data)
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            running_loss = 0.0
+
+print('Finished Training')
