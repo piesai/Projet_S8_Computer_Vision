a03f84bc3c00468742d9ef9b925fb80d492facf4
pierres <sainctavitp@gmail.com>
2025-03-26 15:54:59 +0100

    tout sur CUDA mais c'est lent

--- src/data_clean.py
+++ src/data_clean.py
@@ -1,21 +1,21 @@
 import torchvision
 import torch
-from  torchvision.transforms import v2
+from torchvision.transforms import v2
 import matplotlib.pyplot as plt
 import random as rd
 import numpy as np
-dataset = torchvision.datasets.GTSRB(root = '/home/pierres/Projet_S8/data/',
-                           download=True,
-                           transform = v2.Compose([v2.Resize(size = (200,200)),v2.ToTensor(),v2.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))]))
 
-#split en train test
-data_train, data_test = torch.utils.data.random_split(dataset, [int(0.8*len(dataset)),int(0.2*len(dataset))])
-trainloader = torch.utils.data.DataLoader(data_train, batch_size=16,shuffle = True)
-trainloader_test = torch.utils.data.DataLoader(data_train, batch_size=16,shuffle = True)
+dataset = torchvision.datasets.GTSRB(root='/home/pierres/Projet_S8/data/',
+                                     download=True,
+                                     transform=v2.Compose([v2.Resize(size=(200, 200)), v2.ToTensor(), v2.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]))
 
+data_train, data_test = torch.utils.data.random_split(dataset, [int(0.8 * len(dataset)), int(0.2 * len(dataset))])
+trainloader = torch.utils.data.DataLoader(data_train, batch_size=16, shuffle=True)
+trainloader_test = torch.utils.data.DataLoader(data_train, batch_size=16, shuffle=True)
 
-dataiter = iter(trainloader)
-images, labels = next(dataiter)
-plt.imshow(images[0].permute(1,2,0))
-print(labels)
-plt.show()
+#dataiter = iter(trainloader)
+#images, labels = next(dataiter)
+#images, labels = images.cuda(), labels.cuda()
+#plt.imshow(images[0].permute(1, 2, 0).cpu())
+#print(labels.cpu())
+#plt.show()
--- src/models.py
+++ src/models.py
@@ -11,68 +11,58 @@ class Net(nn.Module):
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(35344, 2048)
-        self.fc2 = nn.Linear(2048, 126)
+        self.fc1 = nn.Linear(35344, 512)
+        self.fc2 = nn.Linear(512, 126)
         self.fc3 = nn.Linear(126, 43)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = torch.flatten(x, 1) # flatten all dimensions except batch
+        x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
-
-net = Net()
+net = Net().cuda()
 
 import torch.optim as optim
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.002, momentum=0.9)
 
-for epoch in range(2):  # loop over the dataset multiple times
-
+for epoch in range(1):
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
-        # get the inputs; data is a list of [inputs, labels]
         inputs, labels = data
-        # zero the parameter gradients
+        inputs, labels = inputs.cuda(), labels.cuda()
         optimizer.zero_grad()
-
-        # forward + backward + optimize
         outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
-
-        # print statistics
         running_loss += loss.item()
-        if i % 10 == 0:    # print every 2000 mini-batches
-            
+        if i % 100 == 0:
             print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
             running_loss = 0.0
-
 
 print('Finished Training')
 
 dataiter = iter(trainloader)
 images, labels = next(dataiter)
+images, labels = images.cuda(), labels.cuda()
 print(labels)
 outputs = net(images)
-outputs = outputs.detach().numpy()
+outputs = outputs.detach().cpu().numpy()
 print([np.argmax(outputs[i]) for i in range(len(outputs))])
 
 correct = 0
 total = 0
-# since we're not training, we don't need to calculate the gradients for our outputs
 with torch.no_grad():
     for data in trainloader_test:
         images, labels = data
-        # calculate outputs by running images through the network
+        images, labels = images.cuda(), labels.cuda()
         outputs = net(images)
-        # the class with the highest energy is what we choose as prediction
         _, predicted = torch.max(outputs, 1)
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
